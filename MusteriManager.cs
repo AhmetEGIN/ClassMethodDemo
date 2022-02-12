dba9ad1355ab74bd7b70590cd613e278521475d0
EGIN <EGIN@LAPTOP-JUH3P2T8>
2022-02-12 19:24:02 +0300

    Add project files.

---
+++ MusteriManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodDemo
+{
+    class MusteriManager
+    {
+        public static void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Sayın {0} {1} bankamızı tercih ettiğiniz için teşekkür ederiz. Id numaranız: {2}",musteri.Ad,musteri.Soyadi,musteri.Id);
+        }
+        public static void Listele(Musteri musteri)
+        {
+            Console.WriteLine("{0} TC kimlik numaralı {1} {2} kişisinin Id numarası: {3}", musteri.TcNo, musteri.Ad, musteri.Soyadi, musteri.Id);
+        }
+        public static void Sil(Musteri musteri)
+        {
+            Console.WriteLine("{0} id numaralı kişi listeden çıkarılmıştır. Sayın {1} {2} sizi tekrardan aramızda görmek isteriz.", musteri.Id, musteri.Ad, musteri.Soyadi);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ClassMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Ad = "Ahmet";
+            musteri1.Soyadi = "EGİN";
+            musteri1.Id = 1;
+            musteri1.TcNo = "98765432134";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Ad = "Engin";
+            musteri2.Soyadi = "DEMİROĞ";
+            musteri2.Id = 2;
+            musteri2.TcNo = "12345678902";
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2 };
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine("Adı: {0} Soyadı: {1} Id No: {2}", musteri.Ad, musteri.Soyadi,musteri.Id);
+            }
+            MusteriManager.Ekle(musteri1);
+            MusteriManager.Listele(musteri1);
+            MusteriManager.Listele(musteri2);
+            MusteriManager.Sil(musteri1);
+            
+        }
+    }
+}
